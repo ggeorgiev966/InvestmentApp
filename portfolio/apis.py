51f0a10baef4d0d425a235f3d9539be3adbada6b
Georgi Georgiev <123512842+ggeorgiev966@users.noreply.github.com>
2024-11-20 00:30:53 +0200

    Updated APIs behaviour and dashboard to show when API request limit reached

---
+++ portfolio/apis.py
@@ -0,0 +1,68 @@
+import requests
+
+LAST_SILVER_PRICE = None
+LAST_BITCOIN_PRICE = None
+LAST_STOCK_PRICES = {}
+
+def get_silver_price():
+    global LAST_SILVER_PRICE
+    api_key = "goldapi-btin6sm3artlym-io"
+    symbol = "XAG"
+    curr = "USD"
+    date = ""
+
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}{date}"
+
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        if response.status_code == 429:
+            return LAST_SILVER_PRICE, "API request limit exceeded. Displaying last available price"
+        data = response.json()
+        LAST_SILVER_PRICE = data['price']
+        return LAST_SILVER_PRICE, None
+    except Exception as e:
+        print(f"Error fetching silver price: {e}")
+        return LAST_SILVER_PRICE, "API request limit exceeded. Displaying last available price"
+
+
+def get_bitcoin_price():
+    global LAST_BITCOIN_PRICE
+    try:
+        url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+        response = requests.get(url)
+        if response.status_code == 429:
+            return LAST_BITCOIN_PRICE, "API request limit exceeded. Displaying last available price"
+        data = response.json()
+        LAST_BITCOIN_PRICE = data['bpi']['USD']['rate']
+        return LAST_BITCOIN_PRICE, None
+    except Exception as e:
+        print(f"Error fetching Bitcoin price: {e}")
+        return LAST_BITCOIN_PRICE, "Error fetching Bitcoin price. Displaying last available price"
+
+
+ALPHA_VANTAGE_API_KEY = '84G9TTF1ZLAGSH0B'  # Replace with your actual API key
+
+def get_stock_price(symbol):
+    global LAST_STOCK_PRICES
+    try:
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}&outputsize=compact'
+        response = requests.get(url)
+        if response.status_code == 429:
+            return LAST_STOCK_PRICES.get(symbol), "API request limit exceeded. Displaying last available price."
+        data = response.json()
+        last_refreshed = max(data['Time Series (Daily)'].keys())  # Get the most recent date
+        current_price = float(data['Time Series (Daily)'][last_refreshed]['4. close'])
+        LAST_STOCK_PRICES[symbol] = current_price
+        return current_price, None
+    except Exception as e:
+        print(f"Error fetching stock price for {symbol}: {e}")
+        return LAST_STOCK_PRICES.get(symbol), "Error fetching stock price. Displaying last available price."
+
+
+
