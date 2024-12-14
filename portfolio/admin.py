1b5a9b096a630bb9880c673e7e06f8283c940354
Georgi Georgiev <123512842+ggeorgiev966@users.noreply.github.com>
2024-12-14 23:02:13 +0200

    Made changes to admin panel

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,25 +1,26 @@
 from django.contrib import admin
 from .models import Stock, Bitcoin, Silver, InvestmentPortfolio
 
+
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ticker', 'price', 'purchased_at', 'user')
+    list_display = ('name', 'ticker', 'price', 'purchased_at', 'user',)
     search_fields = ('name', 'ticker')
     ordering = ('price',)
-    list_filter = ('user',)
+    list_filter = ('user', 'purchased_at')
 
 @admin.register(Bitcoin)
 class BitcoinAdmin(admin.ModelAdmin):
-    list_display = ('quantity', 'purchased_at', 'user')
-    list_filter = ('user',)
+    list_display = ('quantity', 'purchased_at', 'price', 'user',)
+    list_filter = ('user', 'purchased_at')
 
 @admin.register(Silver)
 class SilverAdmin(admin.ModelAdmin):
-    list_display = ('weight', 'purchased_at', 'user')
-    list_filter = ('user',)
+    list_display = ('weight', 'purchased_at', 'price', 'user',)
+    list_filter = ('user', 'purchased_at')
 
 @admin.register(InvestmentPortfolio)
 class PortfolioAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created_at')
-    list_filter = ('created_at',)
-
+    list_display = ('user', 'created_at',)
+    list_filter = ('created_at', 'user')
+    ordering = ('created_at',)
